d943d099b364aaa7a411129432c19d3a1aaa3b1e
Keksiuwu <76953110+keksiqc@users.noreply.github.com>
2022-06-22 15:03:11 +0200

    added new functions

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,13 @@
-declare module "kawaii-api" {
+declare module "kawaiiapi" {
     export class Kawaii {
         public constructor(token: string);
 
-        public get(main: string, endpoint: string, filter: void | string): Promise<string>;
-        public endpoints(main: string): Promise<string>;
+        public get(type: string, endpoint: string, filter: void | string): Promise<string>;
+        public text(text: string): Promise<string>;
+        public image(image: string): Promise<string>;
+        public gif(gif: string): Promise<string>;
+        public random(type: string): Promise<string>;
+        public endpoints(type: string): Promise<string>;
+        public stats(): Promise<object>;
     }
 }
